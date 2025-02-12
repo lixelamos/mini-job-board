6465497c70bb5f3629940bf6d85236211bd7136a
Lixel Amos <48270069+lixelamos@users.noreply.github.com>
2025-02-12 12:49:59 +0300

    update 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import prisma from "@/lib/prisma";
 import H1 from "@/components/ui/h1";
 import JobResults from "@/components/JobResults";
 import { JobFilterValues } from "@/lib/validation";
+import { Metadata } from "next";
+import { metadata } from "./layout";
 
 interface PageProps{
   searchParams:{
@@ -12,24 +14,52 @@ interface PageProps{
     type?:string;
     location?:string;
     remote?:string;
-  }
+  };
 }
 
+function getTitle({q,type,location,remote}:JobFilterValues){
+  const titlePrefix=q
+  ?`${q} jobs`
+  :type
+  ?`${type} developer Jobs for You`
+  :remote
+  ?"Remote Developer Jobs for You"
+  :"All Developer Jobs for You";
+  
+  const titleSuffix=location? `in ${location}`: "";
+  return `${titlePrefix} ${titleSuffix}`
+  }
 
-export default async function Home({
-  searchParams: { q, type, location, remote },
-}: PageProps) {
+  export function generateMetadata({ searchParams }: PageProps): Metadata {
+    const { q, type, location, remote } = searchParams; // Access properties inside the function
+  
+    return {
+      title: `${getTitle({
+        q,
+        type,
+        location,
+        remote: remote === "true",
+      })} | Job Finder`,
+    };
+  }
+  
+
+  export default async function Home({
+     searchParams }
+     : PageProps) {
+    const { q, type, location, remote } = await searchParams; // Await searchParams  
   const filterValues: JobFilterValues = {
     q,
     type,
     location,
-    remote: remote === "true",
+    remote: String(remote) === "true",
   };
+
 
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <H1>Developer Jobs for You</H1>
+        <H1>{getTitle(filterValues)}</H1>
         <p className="text-muted-foreground">Find your dream job.</p>
       </div>
       <section className="flex flex-col gap-4 md:flex-row">
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,12 +1,41 @@
 import { z } from "zod";
+import { jobTypes } from "./job-types";
 
 
+const requiredString =z.string().min(1,"Required");
+const numericRequiredString=z.string().regex(/^\d+$/,"Must be a number");
+
+
+const companyLogoSchema = z
+.custom<File|undefined>()
+.refine(file=>{
+    !file|| (file instanceof File && !file.type.startsWith("image/"))
+},"must be  an image file")
+.refine((file)=>{
+    return !file||file.size<1024*1024*2;
+    },"must be  a file less than 2MB");
+
+
+
+
+export const createJobSchema= z.object({
+    title:requiredString.max(100),
+    type:requiredString.refine(
+        value=>jobTypes.includes(value),
+        "Invalid Job type"
+    ),
+    companyName:requiredString.max(100),
+    companyLogo:companyLogoSchema,
+    description :z.string().max(5000).optional(),
+    salary:numericRequiredString.max(9,"Number cant be longer than 9 digits")
+
+});
 
 export const JobFilterSchema=z.object({
     q:z.string().optional(),
     type:z.string().optional(),
     location:z.string().optional(),
-    remote:z.string().optional(),
+    remote:z.coerce.boolean().optional(),
 
 });
 
