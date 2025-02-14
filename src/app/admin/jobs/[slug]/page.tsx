ffe8ae2b3a3cdc14d0d40df3cc81e55bda2d11f8
Lixel Amos <48270069+lixelamos@users.noreply.github.com>
2025-02-14 20:15:18 +0300

    slug fix1

--- src/app/admin/jobs/[slug]/page.tsx
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -3,22 +3,16 @@ import prisma from "@/lib/prisma";
 import { notFound } from "next/navigation";
 import AdminSidebar from "./AdminSidebar";
 
-// Define the PageProps interface
 interface PageProps {
-  params: {
-    slug: string; // Ensure `slug` is defined as a string
-  };
+  params: { slug: string };
 }
 
-// Ensure Next.js correctly recognizes PageProps
-export default async function Page({ params }: PageProps) {
-  const { slug } = params;
-
+export default async function Page({ params: { slug } }: PageProps) {
   const job = await prisma.job.findUnique({
     where: { slug },
   });
 
-  if (!job) return notFound(); // Ensure `notFound()` is properly returned
+  if (!job) notFound();
 
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
