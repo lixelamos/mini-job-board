777e52ab4320eedaa369d8f63d3bc5af732aaa6d
Lixel Amos <48270069+lixelamos@users.noreply.github.com>
2025-03-10 15:08:49 +0300

    par2

--- src/app/admin/jobs/[slug]/page.tsx
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -6,19 +6,20 @@ import AdminSidebar from "./AdminSidebar";
 interface PageProps {
   params: { slug: string };
 }
-export default async function Page({ params }: Readonly<PageProps>) { // ✅ Use the correct type
+
+export default async function Page({ params }: PageProps) {
   console.log("Params received:", params);
 
   const job = await prisma.job.findUnique({
-    where: { slug: params.slug }, // ✅ Access params.slug correctly
+    where: { slug: params.slug },
   });
 
   if (!job) notFound();
 
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
-      <JobPage job={job?.job} />
-      <AdminSidebar job={job?.job} />
+      <JobPage job={job} />
+      <AdminSidebar job={job} />
     </main>
   );
 }
